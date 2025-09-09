f1af4c7babbd207057aa8f4c9d84296f148aa288
juno-choi <ililil9482@naver.com>
2025-09-09 14:47:37 +0900

    feat: redisson get 실행시 String Codec과 Kryo5 Codec key값이 호환되는지 확인 -> 안됨

--- src/main/java/com/juno/temp/controller/RedisController.java
+++ src/main/java/com/juno/temp/controller/RedisController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.redisson.api.RMap;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -15,9 +18,22 @@ import java.util.concurrent.TimeUnit;
 public class RedisController {
     private final RedissonClient redissonClient;
 
-    @GetMapping("/redis/{key}")
-    public String redis(@PathVariable String key) {
-        redissonClient.getMap("BTC").put("USD", key);
+    @GetMapping("/redis/{value}")
+    public String redis(@PathVariable String value) {
+        RMap<String, String> map = redissonClient.getMap("BTC", StringCodec.INSTANCE);
+        String result = Objects.requireNonNull(map.get("USD"));
+        return result;
+    }
+
+    @PostMapping("/redis/string/{value}")
+    public String postStringRedis(@PathVariable String value) {
+        redissonClient.getMap("BTC", StringCodec.INSTANCE).put("USD", value);
+        return "ok";
+    }
+
+    @PostMapping("/redis/{value}")
+    public String postRedis(@PathVariable String value) {
+        redissonClient.getMap("BTC").put("USD", value);
         return "ok";
     }
 
